210509559f25fa03aca3d64bfa9f4a99446a2fb8
Elin <elin.bjornsson2@gmail.com>
2017-02-21 22:19:56 +0100

    tableColumns are added to the tableView

--- src/Main/MainController.java
+++ src/Main/MainController.java
@@ -1,8 +1,12 @@
 package Main;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -26,8 +30,39 @@ public class MainController {
 
     @FXML
     void newProof(ActionEvent event) {
-    	tabs.push(new Tab(""+counter++));
+    	Tab tab = new Tab(""+counter++);
+    	
+    	TableColumn tableColumnLeft = new TableColumn();
+    	tableColumnLeft.setText("Expression");
+    	tableColumnLeft.setMaxWidth(5000);
+    	tableColumnLeft.setMinWidth(10);
+    	tableColumnLeft.setEditable(true);
+    	//tableColumnLeft.setPrefWidth(356);
+    	
+    	TableColumn tableColumnRight = new TableColumn();
+    	tableColumnRight.setText("Type");
+    	tableColumnRight.setMaxWidth(5000);
+    	tableColumnRight.setMinWidth(10);
+    	tableColumnRight.setEditable(true);
+    	//tableColumnRight.setPrefWidth(356);
+    	
+    	TableView tableView = new TableView();
+    	tableView.setPadding(new Insets(0, 0, 0, 0));
+    	tableView.getColumns().addAll(tableColumnLeft, tableColumnRight);
+    	
+    	AnchorPane anchorPane = new AnchorPane(tableView);
+    	anchorPane.setPrefWidth(714);
+    	anchorPane.setPrefHeight(460);
+    	anchorPane.setPadding(new Insets(0, 0, 0, 0));
+    	anchorPane.setScaleShape(true);
+    	anchorPane.setCacheShape(true);
+    	anchorPane.setCenterShape(true);
+    	
+    	tab.setContent(anchorPane);
+    	tabs.push(tab);
+    	tabPane.setPadding(new Insets(0, 0, 0, 0));
         tabPane.getTabs().add(tabs.peek());
+        
     }
 
     @FXML
