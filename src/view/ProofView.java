d946297be693e06bcfdd63c8333e2b9d8a21a9a2
fsharpasharp <ppooii@gmail.com>
2017-02-27 01:56:50 +0100

    Premises and conclusion bar.

--- src/view/ProofView.java
+++ src/view/ProofView.java
@@ -4,10 +4,9 @@ import java.util.Stack;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Label;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import model.ProofListener;
 import model.ProofTab;
@@ -16,6 +15,7 @@ import model.ProofTab;
 public class ProofView implements ProofListener{
     static final int carryAddOpen = 3; // Magic
     static final int carryAddClose = 5; // Maybe have to calculate this from padding and font size?
+    static final int barHeight = 29;
     private Stack<VBox> stack = new Stack<>();
     private int counter = 1;
     private int carry = 0;
@@ -29,8 +29,39 @@ public class ProofView implements ProofListener{
         newRow();
     };
 
-    public ProofView(TabPane tabPane) {
-        ProofTab tab = new ProofTab("Proof", this);
+    private AnchorPane createBar() {
+        GridPane threeCells = new GridPane();
+        ColumnConstraints column1 = new ColumnConstraints();
+        ColumnConstraints column2 = new ColumnConstraints();
+        ColumnConstraints column3 = new ColumnConstraints();
+        column1.setPercentWidth(45);
+        column2.setPercentWidth(10);
+        column3.setPercentWidth(45);
+        threeCells.getColumnConstraints().addAll(column1,column2,column3);
+        TextField tf1 = new TextField();
+        tf1.getStyleClass().add("myText");
+        TextField tf2 = new TextField("⊢");
+        tf2.getStyleClass().add("myText");
+        tf2.setAlignment(Pos.CENTER);
+        tf2.setEditable(false);
+        TextField tf3 = new TextField();
+        tf3.getStyleClass().add("myText");
+        threeCells.add(tf1, 0, 0);
+        threeCells.add(tf2, 1, 0);
+        threeCells.add(tf3, 2, 0);
+
+        AnchorPane bar = new AnchorPane(threeCells);
+        bar.setTopAnchor(threeCells, 0.0);
+        bar.setRightAnchor(threeCells, 20.0);
+        bar.setLeftAnchor(threeCells, 20.0);
+        bar.setBottomAnchor(threeCells, 0.0);
+        bar.setPrefHeight(barHeight);
+        bar.setMinHeight(barHeight);
+        bar.setMaxHeight(barHeight);
+        return bar;
+    }
+
+    private AnchorPane createProofPane() {
         lineNo = new VBox();
         rows = new VBox();
         lineNo.setFillWidth(true);
@@ -53,14 +84,25 @@ public class ProofView implements ProofListener{
             }
         });
         sp.setContent(ap);
+        AnchorPane proofPane = new AnchorPane(sp);
+        proofPane.setTopAnchor(sp, 0.0);
+        proofPane.setRightAnchor(sp, 0.0);
+        proofPane.setLeftAnchor(sp, 0.0);
+        proofPane.setBottomAnchor(sp, 0.0);
+        return proofPane;
+    }
 
+    public ProofView(TabPane tabPane) {
+        SplitPane sp = new SplitPane(createBar(), createProofPane());
+        sp.setOrientation(Orientation.VERTICAL);
+        sp.setDividerPosition(0, 0.1);
         AnchorPane anchorPane = new AnchorPane(sp);
         anchorPane.setTopAnchor(sp, 0.0);
         anchorPane.setRightAnchor(sp, 0.0);
         anchorPane.setLeftAnchor(sp, 0.0);
         anchorPane.setBottomAnchor(sp, 0.0);
 
-
+        ProofTab tab = new ProofTab("Proof", this);
         tab.setContent(anchorPane);
         tabPane.getTabs().add(tab);
         tabPane.getSelectionModel().select(tab); // Byt till den nya tabben
