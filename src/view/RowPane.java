8b29127cfada516044e98af8dc1ab01e7366c03e
Andreas Widbom <widbom89@gmail.com>
2017-03-09 16:14:22 +0100

    Added RowPane class

---
+++ src/view/RowPane.java
@@ -0,0 +1,34 @@
+package view;
+
+import javafx.scene.layout.BorderPane;
+
+public class RowPane extends BorderPane{
+	private boolean isFirstRowInBox;
+	//nrOfClosingBoxes tracks how many boxes that this row is the last line of
+	private int nrOfClosingBoxes;
+	
+	public RowPane(boolean isFirstRowInBox, int nrOfClosingBoxes){
+		this.isFirstRowInBox = isFirstRowInBox;
+		this.setNrOfClosingBoxes(nrOfClosingBoxes);
+	}
+	
+	public void setIsFirstRowInBox(boolean b){
+		isFirstRowInBox = b;
+	}
+	
+	public boolean getIsFirstRowInBox(){
+		return isFirstRowInBox;
+	}
+
+	public int getNrOfClosingBoxes() {
+		return nrOfClosingBoxes;
+	}
+
+	public void setNrOfClosingBoxes(int nrOfClosingBoxes) {
+		this.nrOfClosingBoxes = nrOfClosingBoxes;
+	}
+	
+	public void incrementNrofClosingBoxes(){
+		nrOfClosingBoxes++;
+	}
+}
