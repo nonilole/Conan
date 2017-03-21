7fc0282f0baead96589d6739238fbbcf5545550b
Henry Ly <Henry Ly>
2017-03-21 19:24:48 +0100

    Helpview for displaying inference rules

--- src/view/HelpView.java
+++ src/view/HelpView.java
@@ -1,5 +1,46 @@
 package view;
 
-class HelpView {
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+
+class HelpView implements View {
+    //The tab object of this view
+    private Tab tab;
+
+    /**
+     * Adds the content for showing the inference rules to the TabPane in the proof
+     * @param tabPane
+     *
+     */
+    public HelpView(TabPane tabPane) {
+    //TODO adjust the image based on screen size
+
+            //load the image
+            Image image = new Image("inferenceRules.jpg");
+
+            //image resizing
+            ImageView iv1 = new ImageView();
+            iv1.setImage(image);
+            iv1.setPreserveRatio(true);
+            iv1.setSmooth(true);
+            iv1.fitHeightProperty().bind(tabPane.heightProperty());
+            iv1.fitWidthProperty().bind(tabPane.widthProperty());
+
+            //putting the image on a scrollpane
+            ScrollPane sp=new ScrollPane();
+            Tab tab = new Tab();
+            tab.setText("Inference Rules");
+            sp.setContent(iv1);
+            tabPane.getTabs().add(tab);
+            tab.setContent(sp);
+            tabPane.getSelectionModel().select(tab);
+
+    }
+
+    @Override
+    public Tab getTab() {
+        return tab;
+    }
 }
