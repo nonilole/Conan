3c6b13093e474db93f81bb68c9a6731c66bfcc7e
fsharpasharp <ppooii@gmail.com>
2017-03-31 11:03:04 +0200

    Fix master

---
+++ src/model/ProofListener.java
@@ -0,0 +1,13 @@
+package model;
+
+public interface ProofListener {
+    public void boxOpened();
+    public void boxClosed();
+    public void rowUpdated(boolean wellFormed, int lineNo);
+    public void conclusionReached(boolean correct, int lineNo);
+    public void rowDeleted(int rowNr);
+    public void rowInserted(int rowNo, BoxReference order);
+    public void rowAdded();
+    
+}
+
