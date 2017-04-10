935918a146d235ea681008b7afe02a1c6219f1ef
Elin <elin.bjornsson2@gmail.com>
2017-04-11 00:32:45 +0200

    Added the class for verification of disjuntion elimination with some comments of what needs to be done.

---
+++ src/model/rules/DisjunctionElim.java
@@ -0,0 +1,33 @@
+package model.rules;
+
+import model.Box;
+
+public class DisjunctionElim implements Rule{
+
+	@Override
+	public boolean hasCompleteInfo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void updateReference(int index, String newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean verify(Box data, int rowIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	//kolla att en rad och två intervall anges. 
+	//kolla så att alla rader/boxar som används är verified. 
+	//kolla så att båda boxar avslutas med det som ska bevisas.
+	//kolla så att den ena boxen börjar med ena sidan av ursprungliga OR-uttrycket.
+	//kolla att den andra boxen börjar med andra sidan av OR-uttrycket. 
+	
+	//fundering: hur har vi koll på boxarna just nu? 
+
+}
