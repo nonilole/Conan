1fcaf257b1df280b671b74d15e4cd32b098b78eb
fsharpasharp <ppooii@gmail.com>
2017-04-23 16:02:07 +0200

    Testing icon

--- src/start/Main.java
+++ src/start/Main.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -16,8 +17,9 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sketch.fxml"));
         Scene scene = new Scene(root, 1200, 600);
-        scene.getStylesheets().add("minimalistStyle.css");
+        scene.getStylesheets().add("style.css");
         stage.setTitle("Conan");
+        stage.getIcons().add(new Image("file:icon.png"));
         stage.setScene(scene);
         stage.show();
     }
