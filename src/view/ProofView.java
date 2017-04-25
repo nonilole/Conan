42482a1dedf8027f9311f1df1d1b5772a535ea9c
fsharpasharp <ppooii@gmail.com>
2017-04-25 09:47:50 +0200

    Resolving merge conflict

--- src/view/ProofView.java
+++ src/view/ProofView.java
@@ -226,6 +226,7 @@ public class ProofView extends Symbolic implements ProofListener, View {
         ruleMap.put("MT", 2);
         ruleMap.put("LEM", 0);
         ruleMap.put("PBC", 1);
+        ruleMap.put("¬¬I", 1);
     }
 
     /* Controller begin */
