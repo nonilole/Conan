e6d9f9b6861b6bf2338b294a8b65d892dc98918f
fsharpasharp <ppooii@gmail.com>
2017-04-26 15:48:34 +0200

    Added ctrl-left and ctrl-right navigation

--- src/view/RowPane.java
+++ src/view/RowPane.java
@@ -23,6 +23,8 @@ import static view.ViewUtil.checkShortcut;
 
 public class RowPane extends BorderPane {
     final static KeyCombination shiftEnter = new KeyCodeCombination(KeyCode.ENTER, KeyCombination.SHIFT_DOWN);
+    final static KeyCombination ctrlLeft = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.CONTROL_DOWN);
+    final static KeyCombination ctrlRight = new KeyCodeCombination(KeyCode.RIGHT, KeyCombination.CONTROL_DOWN);
     final static KeyCombination ctrlB = new KeyCodeCombination(KeyCode.B, KeyCombination.CONTROL_DOWN);
     final static KeyCombination ctrlD = new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_DOWN);
     private static final HashMap<String, Integer> ruleMap;
@@ -60,6 +62,7 @@ public class RowPane extends BorderPane {
     private int numberOfPrompts;
     private boolean isFirstRowInBox;
     private int nrOfClosingBoxes;
+    private RulePane rulePane;
 
     // Always call init after adding RowPane to rList
     public RowPane(boolean isFirstRowInBox, int nrOfClosingBoxes) {
@@ -73,7 +76,8 @@ public class RowPane extends BorderPane {
         tfExpression.getStyleClass().add("myText");
         tfExpression.setPrefWidth(580);
         this.setCenter(tfExpression);
-        this.setRight(new RulePane());
+        rulePane = new RulePane();
+        this.setRight(rulePane);
         this.setCache(true);
         this.setCacheShape(true);
         this.setCacheHint(CacheHint.SPEED);
@@ -148,7 +152,7 @@ public class RowPane extends BorderPane {
             } catch (InstantiationException e) {
                 e.printStackTrace();
             }
-            setPrompts(ruleMap.getOrDefault(newValue, -1));
+            setPrompts(ruleMap.getOrDefault(newValue, 0));
         });
         for (int i = 0; i < 3; i++) {
             new PromptFocus(getRulePrompt(i), pv, rList, i);
@@ -202,8 +206,7 @@ public class RowPane extends BorderPane {
     }
 
     public TextField getRule() {
-        RulePane rulePane = (RulePane) this.getRight();
-        return (TextField) rulePane.getChildren().get(0);
+        return rulePane.getRule();
     }
 
     public void setRule(String s) {
@@ -211,7 +214,8 @@ public class RowPane extends BorderPane {
     }
 
     private TextField getClosestPromptFromLeft(int index) {
-        if (index > this.numberOfPrompts - 1)
+        System.out.println(index);
+        if (index >= this.numberOfPrompts)
             index = this.numberOfPrompts - 1;
         if (index == -1)
             return getRule();
@@ -223,8 +227,7 @@ public class RowPane extends BorderPane {
     }
 
     public TextField getRulePrompt(int index) {
-        RulePane rulePane = (RulePane) this.getRight();
-        return (TextField) rulePane.getChildren().get(1 + index);
+        return rulePane.getRulePrompt(index);
     }
 
     private void hideAndClearPrompts() {
@@ -237,20 +240,13 @@ public class RowPane extends BorderPane {
     private void setPrompts(int n) {
         hideAndClearPrompts();
         this.numberOfPrompts = n;
-        TextField tf;
         switch (n) {
             case 3:
                 getRulePrompt(2).setVisible(true);
-                tf = getRulePrompt(2);
-                tf.setPromptText("Rows");
             case 2:
                 getRulePrompt(1).setVisible(true);
-                tf = getRulePrompt(1);
-                tf.setPromptText("Rows");
             case 1:
                 getRulePrompt(0).setVisible(true);
-                tf = getRulePrompt(0);
-                tf.setPromptText("Rows");
                 break;
             default:
                 break;
@@ -261,24 +257,28 @@ public class RowPane extends BorderPane {
         private HotkeyMapper(TextField trigger, ProofView pv, List<RowPane> rList) {
             addFocusListener(trigger, pv);
             trigger.setOnKeyPressed(new EventHandler<KeyEvent>() {
-                public void handle(KeyEvent ke) {
+                public void handle(KeyEvent key) {
                     int index = rList.indexOf(RowPane.this);
-                    if (ctrlD.match(ke)) {
+                    if (ctrlD.match(key)) {
                         pv.deleteRow(index + 1);
                     }
-                    if (ctrlB.match(ke)) {
+                    if (ctrlB.match(key)) {
                         pv.insertNewBox(index + 1);
-                    } else if (shiftEnter.match(ke)) {
+                    } else if (ctrlLeft.match(key)) {
+                        focusLeft();
+                    } else if (ctrlRight.match(key)) {
+                        focusRight();
+                    } else if (shiftEnter.match(key)) {
                         pv.addRowAfterBox(index + 1);
                         focus(pv, rList, index + 1);
-                    } else if (ke.getCode() == KeyCode.ENTER) {
+                    } else if (key.getCode() == KeyCode.ENTER) {
                         pv.insertNewRow(index + 1, BoxReference.AFTER);
                         focus(pv, rList, index + 1);
-                    } else if (ke.getCode() == KeyCode.DOWN) {
+                    } else if (key.getCode() == KeyCode.DOWN) {
                         if (index + 1 >= rList.size())
                             pv.addRowAfterBox(index + 1);
                         focus(pv, rList, index + 1);
-                    } else if (ke.getCode() == KeyCode.UP) {
+                    } else if (key.getCode() == KeyCode.UP) {
                         if (index - 1 >= 0) {
                             focus(pv, rList, index - 1);
                         }
@@ -288,6 +288,8 @@ public class RowPane extends BorderPane {
         }
 
         public abstract void focus(ProofView pv, List<RowPane> rList, int index);
+        public abstract void focusLeft();
+        public abstract void focusRight();
     }
 
     private class RuleFocus extends HotkeyMapper {
@@ -299,6 +301,16 @@ public class RowPane extends BorderPane {
         public void focus(ProofView pv, List<RowPane> rList, int index) {
             rList.get(index).getRule().requestFocus();
         }
+
+        @Override
+        public void focusLeft() {
+            getExpression().requestFocus();
+        }
+
+        @Override
+        public void focusRight() {
+            getClosestPromptFromLeft(0).requestFocus();
+        }
     }
 
     private class ExpressionFocus extends HotkeyMapper {
@@ -309,6 +321,15 @@ public class RowPane extends BorderPane {
         @Override
         public void focus(ProofView pv, List<RowPane> rList, int index) {
             rList.get(index).getExpression().requestFocus();
+        }
+
+        @Override
+        public void focusLeft() {
+        }
+
+        @Override
+        public void focusRight() {
+            getRule().requestFocus();
         }
     }
 
@@ -323,6 +344,21 @@ public class RowPane extends BorderPane {
         @Override
         public void focus(ProofView pv, List<RowPane> rList, int index) {
             rList.get(index).getClosestPromptFromLeft(prompt).requestFocus();
+        }
+
+        @Override
+        public void focusLeft() {
+            if (prompt == 0) {
+                getRule().requestFocus();
+            } else {
+                getClosestPromptFromLeft(prompt - 1).requestFocus();
+            }
+
+        }
+
+        @Override
+        public void focusRight() {
+            getRulePrompt(prompt + 1).requestFocus();
         }
     }
 }
--- src/view/RulePane.java
+++ src/view/RulePane.java
@@ -3,32 +3,33 @@ package view;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 
+import java.util.ArrayList;
+
 // Only GUI no logic"
 public class RulePane extends FlowPane {
+    private TextField rule;
+    private ArrayList<TextField> rulePrompts;
+    public TextField getRulePrompt(int i) {
+        return rulePrompts.get(i);
+    }
+    public TextField getRule() {
+        return rule;
+    }
     public RulePane() {
+        rulePrompts = new ArrayList<TextField>(3);
         this.setMaxWidth(340);
-        TextField tfRule = new TextField();
-        tfRule.setPromptText("Rule");
-        TextField tfRulePrompt1 = new TextField();
-        TextField tfRulePrompt2 = new TextField();
-        TextField tfRulePrompt3 = new TextField();
-        //set the rule prompts to invisible
-        tfRulePrompt1.setVisible(false);
-        tfRulePrompt2.setVisible(false);
-        tfRulePrompt3.setVisible(false);
-        tfRule.setId("rightTextField");
-        tfRulePrompt1.setId("rulePromt1tf");
-        tfRulePrompt2.setId("rulePromt2tf");
-        tfRulePrompt3.setId("rulePromt3tf");
-        tfRule.getStyleClass().add("myText");
-        tfRulePrompt1.getStyleClass().add("myText");
-        tfRulePrompt2.getStyleClass().add("myText");
-        tfRulePrompt3.getStyleClass().add("myText");
-        tfRule.setMaxWidth(100);
-        tfRulePrompt1.setMaxWidth(80);
-        tfRulePrompt2.setMaxWidth(80);
-        tfRulePrompt3.setMaxWidth(80);
-        //adding the textfield for the rule and the rulepromts
-        this.getChildren().addAll(tfRule, tfRulePrompt1, tfRulePrompt2, tfRulePrompt3);
+        rule = new TextField();
+        rule.getStyleClass().add("myText");
+        rule.setPromptText("Rule");
+        rule.setId("rightTextField");
+        rule.setMaxWidth(100);
+        for (int i = 0; i < 3; i++) {
+            TextField temp = new TextField();
+            temp.setVisible(false);
+            temp.getStyleClass().add("myText");
+            temp.setMaxWidth(80);
+            rulePrompts.add(temp);
+        }
+        this.getChildren().addAll(rule, rulePrompts.get(0), rulePrompts.get(1), rulePrompts.get(2));
     }
 }
