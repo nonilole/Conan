e8b8cdb3fe72b5342cef84e904b34e9ad091a007
fsharpasharp <ppooii@gmail.com>
2017-04-28 11:19:15 +0200

    Fix crash bug

--- src/model/Proof.java
+++ src/model/Proof.java
@@ -178,8 +178,8 @@ public class Proof implements Serializable{
 
     public boolean insertBox(int rowIndex) {
         ProofRow pr = proofData.getRow(rowIndex);
-        if (!(pr.getParent().isTopLevelBox())) {
-            if (pr.getParent() != null && pr.getParent().getRow(0) == pr) {
+        if (pr.getParent() != null && !pr.getParent().isTopLevelBox()) {
+            if (pr.getParent().getRow(0) == pr) {
                 return false; // Don't insert box if it's the first row in a box.
             }
         }
