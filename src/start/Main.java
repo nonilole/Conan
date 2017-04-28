f03f7d44bb6f5b48bb41127df338b54db97dd4d4
Elin <elin.bjornsson2@gmail.com>
2017-04-28 16:35:25 +0200

    increase the size of the program

--- src/start/Main.java
+++ src/start/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sketch.fxml"));
-        Scene scene = new Scene(root, 1200, 600);
+        Scene scene = new Scene(root, 1200, 700);
         scene.getStylesheets().add("minimalistStyle.css");
 //        scene.getStylesheets().add("gruvjan.css");
         stage.setTitle("Conan");
