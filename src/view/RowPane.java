d861e468ae66a4c3d4cf1f83a5ce604618152f0e
Mispelled <mispelled@outlook.com>
2017-04-29 18:42:48 +0200

    Fixed edit menu items calling the wrong functions

--- src/view/RowPane.java
+++ src/view/RowPane.java
@@ -314,7 +314,7 @@ public class RowPane extends BorderPane {
                     } else if (key.getCode() == ENTER) {
                         pv.insertNewRow(index + 1, BoxReference.AFTER);
                         focus(pv, rList, index + 1);
-                    } else if (key.getCode() == KeyCode.DOWN) {
+                    }  else if (key.getCode() == KeyCode.DOWN) {
                         if (index + 1 >= rList.size())
                             pv.addRowAfterBox(index + 1);
                         focus(pv, rList, index + 1);
