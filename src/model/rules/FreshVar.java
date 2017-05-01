b9ed1f86dae453dbc2222185c58aa2318a9fe74b
fsharpasharp <ppooii@gmail.com>
2017-05-01 20:31:39 +0200

    Changed latex-name

--- src/model/rules/FreshVar.java
+++ src/model/rules/FreshVar.java
@@ -63,18 +63,9 @@ public class FreshVar extends Rule {
         return null;
     }
 
-    /*@Override
-    public boolean verify(Box data, int rowIndex) {
-        ProofRow row = data.getRow(rowIndex);
-        if (row.getParent().getRow(0) != row) return false;
-        if (row.getFormula() instanceof FreshVarFormula == false) return false;
-        //TODO: check that the var is actually fresh!
-        return true;
-    }*/
-
     @Override
     public String toString() {
-        return "Fresh var.";
+        return "";
     }
 
 	@Override
