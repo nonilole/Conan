ac39d29c0064d5aeaa4288c1d657692b1c0a4d3c
fsharpasharp <ppooii@gmail.com>
2017-05-01 17:57:54 +0200

    MT prompt agnostic

--- src/model/rules/ModusTollens.java
+++ src/model/rules/ModusTollens.java
@@ -10,6 +10,7 @@ public class ModusTollens extends Rule {
 
     private Integer rowRef1;
     private Integer rowRef2;
+    private Implication implication;
 
     @Override
     public boolean hasCompleteInfo() {
@@ -37,32 +38,37 @@ public class ModusTollens extends Rule {
         }
     }
 
-    @Override
-    public boolean verifyReferences(Box data, int rowIndex) {
-        if (!(data.isInScopeOf(rowRef1, rowIndex))) return false;
-        if (!(data.isInScopeOf(rowRef2, rowIndex))) return false;
-        Formula referencedRow1 = data.getRow(rowRef1).getFormula();
-        Formula referencedRow2 = data.getRow(rowRef2).getFormula();
-        if (!(referencedRow1 instanceof Implication)) return false;
-        if (!(referencedRow2 instanceof Negation)) return false;
+    private boolean verifySecondArgImpl(Formula referencedRow1, Formula referencedRow2) {
+        if (!(referencedRow2 instanceof Implication)) return false;
+        if (!(referencedRow1 instanceof Negation)) return false;
+        Implication implRef = (Implication) referencedRow2;
+        Negation negRef = (Negation) referencedRow1;
+        if (!(negRef.formula.equals(implRef.rhs))) return false;
+        implication = implRef;
         return true;
     }
 
     @Override
-    public boolean verifyRow(Box data, int rowIndex) {
+    public boolean verifyReferences(Box data, int rowIndex) {
+        implication = null;
+        if (!(data.isInScopeOf(rowRef1, rowIndex))) return false;
+        if (!(data.isInScopeOf(rowRef2, rowIndex))) return false;
         Formula referencedRow1 = data.getRow(rowRef1).getFormula();
-        Implication implRef = (Implication) referencedRow1;
+        Formula referencedRow2 = data.getRow(rowRef2).getFormula();
+        return (verifySecondArgImpl(referencedRow1, referencedRow2) || verifySecondArgImpl(referencedRow2, referencedRow1));
+    }
+
+    @Override
+    public boolean verifyRow(Box data, int rowIndex) {
         Formula result = data.getRow(rowIndex).getFormula();
         if (result instanceof Negation == false) return false;
         Negation negResult = (Negation) result;
-        return implRef.lhs.equals(negResult.formula);
+        return implication.lhs.equals(negResult.formula);
     }
 
     @Override
     public Formula generateRow(Box data) {
-        Formula referencedRow2 = data.getRow(rowRef1).getFormula();
-        Implication implRef = (Implication) referencedRow2;
-        return new Negation(implRef.lhs);
+        return new Negation(implication.lhs);
     }
 
     @Override
