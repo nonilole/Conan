265805bba1ba6bf84e5000741e55089721770d30
fsharpasharp <ppooii@gmail.com>
2017-05-02 09:31:37 +0200

    Added additional replacements

--- src/view/ViewUtil.java
+++ src/view/ViewUtil.java
@@ -37,11 +37,12 @@ public class ViewUtil {
         newValue = newValue.replaceAll("&|an", "∧");
         newValue = newValue.replaceAll("->|im", "→");
         newValue = newValue.replaceAll("fa|fo", "∀");
-        newValue = newValue.replaceAll("or", "∨");
+        newValue = newValue.replaceAll("or|\\|", "∨");
         newValue = newValue.replaceAll("ex|te|th", "∃");
         newValue = newValue.replaceAll("co|bo", "⊥");
+        newValue = newValue.replaceAll("⊥py", "Copy");
         newValue = newValue.replaceAll("premise", "Premise");
-        newValue = newValue.replaceAll("ass", "Ass");
+        newValue = newValue.replaceAll("ass.", "Ass.");
         newValue = newValue.replaceAll("fresh", "Fresh");
         newValue = newValue.replaceAll("0", "₀");
         newValue = newValue.replaceAll("1", "₁");
