1889cb441cfa8ff3f696f1c432e662f78d5a2bb2
Elin <elin.bjornsson2@gmail.com>
2017-05-25 15:22:07 +0200

    added a dialog box when you close the program

--- src/start/Main.java
+++ src/start/Main.java
@@ -1,7 +1,9 @@
 package start;
 
 import javafx.application.Application;
+
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -9,27 +11,67 @@ import javafx.scene.Parent;
 
 import java.util.prefs.Preferences;
 
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.*;
+import javafx.stage.WindowEvent;
+
+import java.util.Optional;
+
 public class Main extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sketch.fxml"));
-        Scene scene = new Scene(root, 1200, 700);
-        Preferences prefs = Preferences.userRoot().node("General");
-        switch (prefs.getInt("theme",0)) {
-            case 0:
-                scene.getStylesheets().add("minimalistStyle.css");
-                break;
-            case 1:
-                scene.getStylesheets().add("gruvjan.css");
-                break;
+	
+	private Stage mainStage;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("sketch.fxml"));
+		Scene scene = new Scene(root, 1200, 700);
+		Preferences prefs = Preferences.userRoot().node("General");
+		switch (prefs.getInt("theme",0)) {
+		case 0:
+			scene.getStylesheets().add("minimalistStyle.css");
+			break;
+		case 1:
+			scene.getStylesheets().add("gruvjan.css");
+			break;
+		}
+		stage.setTitle("Conan");
+		stage.getIcons().add(new Image("icon.png"));
+		stage.setScene(scene);
+		stage.show();
+		
+		stage.setOnCloseRequest(confirmCloseEventHandler);
+		
+		//Button closeButton = new Button("Close Application");
+		//closeButton.setOnAction(event ->
+		  //      stage.fireEvent( new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST)));
+	}
+	
+	private EventHandler<WindowEvent> confirmCloseEventHandler = event -> {
+        Alert closeConfirmation = new Alert(
+                Alert.AlertType.CONFIRMATION,
+                "You may have unsaved proofs, are you sure you want to exit?"
+        );
+        Button exitButton = (Button) closeConfirmation.getDialogPane().lookupButton(
+                ButtonType.OK
+        );
+        exitButton.setText("Exit");
+        closeConfirmation.setHeaderText("Confirm Exit");
+        closeConfirmation.initModality(Modality.APPLICATION_MODAL);
+        closeConfirmation.initOwner(mainStage);
+
+        Optional<ButtonType> closeResponse = closeConfirmation.showAndWait();
+        if (!ButtonType.OK.equals(closeResponse.get())) {
+            event.consume();
         }
-        stage.setTitle("Conan");
-        stage.getIcons().add(new Image("icon.png"));
-        stage.setScene(scene);
-        stage.show();
-    }
- public static void main(String[] args) {
-        launch(args);
-    }
+    };
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
